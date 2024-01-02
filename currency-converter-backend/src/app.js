b85ce6fe6eaad6a2f98f73b6bf9cfe18fe58d1f2
ThokatiNavyaSree <navyasree2710@gmail.com>
2024-01-02 20:59:52 +0530

    commit

--- currency-converter-backend/src/app.js
+++ currency-converter-backend/src/app.js
@@ -8,11 +8,6 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-// Parse environment variables
-const sourceCrypto = process.env.SOURCE_CRYPTO || 'defaultCrypto';
-const amount = parseFloat(process.env.AMOUNT) || 0;
-const targetCurrency = process.env.TARGET_CURRENCY || 'defaultCurrency';
-
 app.use('/api', cryptoRoutes);
 
 app.listen(PORT, () => {
--- currency-converter-backend/src/routes/cryptoRoutes.js
+++ currency-converter-backend/src/routes/cryptoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const cryptoController = require('../controllers/cryptoController');
 
-router.get('api/top-cryptos', cryptoController.getTopCryptos);
-router.get('api/convert', cryptoController.validateInput, cryptoController.convertCurrency);
+router.get('/top-cryptos', cryptoController.getTopCryptos);
+router.get('/convert', cryptoController.validateInput, cryptoController.convertCurrency);
 
 module.exports = router;
